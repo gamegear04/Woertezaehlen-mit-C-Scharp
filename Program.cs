23b58be775e32af443097b43d41e64533117a263
Darius Korzeneiwski <darius_korzeniewski@web.de>
2016-09-23 09:00:21 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace Dict
+{
+	class Program
+	{
+			// Mit Dictarnary dublicate zu lassen
+			Dictionary<string, int> wordsDictionary = new Dictionary<string, int>();
+
+			public static void Main(string[] args)
+			{
+				string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "LargeCanterburyCorpus.txt");
+				Program p = new Program();
+				p.CreateDictionary(filePath);
+				p.OutputStatistics();
+
+				Console.ReadKey(true);
+			}
+
+			private void CreateDictionary(string filePath)
+			{
+				wordsDictionary.Clear();
+
+				string fileText = File.ReadAllText(filePath);
+				string[] words = fileText.Split(new char[] { ' ', '\t', '\r', '\n', '.', ',', '?', ';', ':' }, StringSplitOptions.RemoveEmptyEntries);
+
+				foreach (string word in words)
+				{
+					if (wordsDictionary.ContainsKey(word))
+						wordsDictionary[word] = wordsDictionary[word] + 1;
+					else
+					{
+						wordsDictionary.Add(word, 1);
+					}
+				}
+			}
+
+
+		public void OutputStatistics()
+		{
+			var orderedPairs = wordsDictionary.OrderBy(k => k.Value);
+			foreach (KeyValuePair<string, int> kvp in orderedPairs)
+			{
+				Console.WriteLine("{0} - {1}", kvp.Key, kvp.Value);
+			}
+		}
+	}
+}
